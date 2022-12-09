069a9cc4c9b23c7bf32cb7b6aeaa46ba086da9cf
szpicus <szpicus@interia.pl>
2022-12-09 17:32:42 +0100

    init

---
+++ AdventOfCode/Day9/Day9Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOFCode.Day9
+{
+    public  class Day9Solution : BaseUtils
+    {
+        public Day9Solution() : base("input9.txt") { }
+
+        static Direction Left = new Direction(0, -1);
+        static Direction Right = new Direction(0, 1);
+        static Direction Up = new Direction(-1, 0);
+        static Direction Down = new Direction(1, 0);
+        public override void SolveIssue()
+        {
+            HashSet<string> visitedPlaces = new();
+            visitedPlaces.Add("00");
+            var commands = Data.SplitByEndOfLine();
+            Location head,tail = new();
+            foreach (var line in commands)
+            {
+                var command = line.SplitBySpace();
+
+            }
+        }
+    }
+    record Direction(int x, int y);
+
+    public class Location
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Location()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+
+}
